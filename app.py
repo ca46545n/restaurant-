2adaecf949ba89bc61434de85701f1797e685d3b
ca46545n <ca46545n@pace.edu>
2017-12-08 16:30:29 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask,jsonify
+import json
+
+app = Flask(__name__)
+jData = json.loads(open('./resturant.json').read())
+data=jData["Resturant"]
+# print (data[0]["id"])
+
+@app.route('/')
+def hello_world():
+    return 'Flask Dockerized'
+
+@app.route('/restaurant/')
+@app.route('/restaurant/<string:id>/')
+def hello_world1(id=''):
+        if not id:
+            v = 'for blank'
+            print('for blank')
+            rdata = jsonify(data)
+        else:
+            v = 'for not blank'
+            print('for not blank')
+            rdata = jsonify(v)
+        for f in data:
+            if f["id"] == id:
+                rdata = jsonify(f)
+        return rdata
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
